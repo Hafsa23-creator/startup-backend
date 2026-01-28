9c3f9ab76ed57042ebbada03ccc723fd72b7758e
Hafsa Megrous <hafsamegrous14@gmail.com>
2026-01-28 02:57:21 +0100

    add missing models/user.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import Project from "../models/Project.js";
 import multer from "multer";
 
