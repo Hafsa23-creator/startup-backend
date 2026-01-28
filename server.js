5c3da62fb7a26dc7ee6a8325314722465445fb4b
Hafsa Megrous <hafsamegrous14@gmail.com>
2026-01-28 02:51:00 +0100

    add port log for Render

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ app.use("/api/partnerships", partnershipRoutes);
 app.use("/api/reviews", reviewRoutes);
 
 // Start server
+console.log("Trying to listen on port:", process.env.PORT || 5000);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
