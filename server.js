52a740d5a1d505afddcb5e5317d64eb4a0b5bbcb
Hafsa Megrous <hafsamegrous14@gmail.com>
2026-04-03 01:46:55 +0200

    Update API URLs to Vercel backend

--- server.js
+++ server.js
@@ -56,5 +56,4 @@ app.use("/api/reviews", reviewRoutes);
 app.use("/api/stats", statsRoutes);
 app.use("/api/ratings", ratingRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
